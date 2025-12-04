0629936db3aaf6f04816e10585b850824fc08104
FelixBedoya15 <wappyinteractivo@gmail.com>
2025-12-04 17:05:39 -0500

    s

--- client/src/components/Liva/LivePage.tsx
+++ client/src/components/Liva/LivePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import LiveEditor from './Editor/LiveEditor';
 import LiveAnalysisModal from './LiveAnalysisModal';
 import { Video, Save } from 'lucide-react';
