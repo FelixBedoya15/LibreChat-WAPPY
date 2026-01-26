9ef53096506c1e9788dfe40e64f4914b449c3aaa
FelixBedoya15 <wappyinteractivo@gmail.com>
2026-01-26 11:15:35 -0500

    s

--- client/src/components/Nav/SettingsTabs/Account/Account.tsx
+++ client/src/components/Nav/SettingsTabs/Account/Account.tsx
@@ -4,13 +4,14 @@ import DeleteAccount from './DeleteAccount';
 import Avatar from './Avatar';
 import EnableTwoFactorItem from './TwoFactorAuthentication';
 import BackupCodesItem from './BackupCodesItem';
-import EditProfileModal from './EditProfileModal';
+// import EditProfileModal from './EditProfileModal';
 import { useAuthContext, useLocalize } from '~/hooks';
 
 function Account() {
   const { user } = useAuthContext();
   const localize = useLocalize();
-  const [showEditModal, setShowEditModal] = useState(false);
+  // const [showEditModal, setShowEditModal] = useState(false);
+  const showEditModal = false; // Placeholder
 
   return (
     <div className="flex flex-col gap-3 p-1 text-sm text-text-primary">
@@ -20,14 +21,14 @@ function Account() {
       <div className="pb-3">
         <Avatar />
       </div>
-      <div className="pb-3">
+      {/* <div className="pb-3">
         <button
           onClick={() => setShowEditModal(true)}
           className="btn btn-neutral"
         >
           {localize('com_ui_edit_profile') || 'Edit Profile'}
         </button>
-      </div>
+      </div> */}
       {user?.provider === 'local' && (
         <>
           <div className="pb-3">
@@ -43,7 +44,7 @@ function Account() {
       <div className="pb-3">
         <DeleteAccount />
       </div>
-      <EditProfileModal isOpen={showEditModal} onClose={() => setShowEditModal(false)} />
+      {/* <EditProfileModal isOpen={showEditModal} onClose={() => setShowEditModal(false)} /> */}
     </div>
   );
 }
