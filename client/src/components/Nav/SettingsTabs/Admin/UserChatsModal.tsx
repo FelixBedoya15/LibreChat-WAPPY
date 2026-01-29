6032b7d0423bbb6a1fbc2e0853160b91c11cd1e2
FelixBedoya15 <wappyinteractivo@gmail.com>
2026-01-29 16:16:24 -0500

    s

--- client/src/components/Nav/SettingsTabs/Admin/UserChatsModal.tsx
+++ client/src/components/Nav/SettingsTabs/Admin/UserChatsModal.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
 import { useToastContext, Spinner } from '@librechat/client';
 import { useLocalize } from '~/hooks';
 import { getLatestText } from '~/utils';
