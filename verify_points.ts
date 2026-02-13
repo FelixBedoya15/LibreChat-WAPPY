6efc629c8777d3d40de620779ab16da365e7ee59
FelixBedoya15 <wappyinteractivo@gmail.com>
2026-02-13 08:27:26 -0500

    s

--- verify_points.ts
+++ verify_points.ts
@@ -1,6 +1,7 @@
-
 import { ARTICLE_16_STANDARDS, getTotalPoints } from './client/src/components/SGSST/checklistData';
+import { AUDITORIA_ITEMS } from './client/src/components/SGSST/auditoriaData';
 
+console.log('--- CHECKLIST DATA (Diagnóstico) ---');
 console.log('Total Items:', ARTICLE_16_STANDARDS.length);
 console.log('Total Points:', getTotalPoints(ARTICLE_16_STANDARDS));
 
@@ -11,6 +12,9 @@ ARTICLE_16_STANDARDS.forEach(item => {
     groups[key] = (groups[key] || 0) + item.points;
 });
 console.log('Points by Category:', groups);
+
+console.log('\n--- AUDITORIA DATA (Auditoría) ---');
+console.log('Total Items:', AUDITORIA_ITEMS.length);
 
 const subGroups: Record<string, number> = {};
 ARTICLE_16_STANDARDS.forEach(item => {
