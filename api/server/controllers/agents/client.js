053ca53caabf6379b7ca60aa7ef4045b96e4ce9b
FelixBedoya15 <wappyinteractivo@gmail.com>
2026-02-18 20:07:53 -0500

    s

--- api/server/controllers/agents/client.js
+++ api/server/controllers/agents/client.js
@@ -776,7 +776,7 @@ class AgentClient extends BaseClient {
           thread_id: this.conversationId,
           last_agent_index: this.agentConfigs?.size ?? 0,
           user_id: this.user ?? this.options.req.user?.id,
-          hide_sequential_outputs: this.options.agent.hide_sequential_outputs ?? true,
+          hide_sequential_outputs: true,
           requestBody: {
             messageId: this.responseMessageId,
             conversationId: this.conversationId,
