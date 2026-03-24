4532fe3375d5ca7fb6f6eb18b20221c903f659c8
FelixBedoya15 <wappyinteractivo@gmail.com>
2026-03-24 15:26:41 -0500

    s

--- client/src/components/Nav/Settings.tsx
+++ client/src/components/Nav/Settings.tsx
@@ -218,7 +218,7 @@ export default function Settings({ open, onOpenChange, activeTab: initialTab }: 
           <div className={cn('fixed inset-0 flex w-screen items-center justify-center p-4')}>
             <DialogPanel
               className={cn(
-                'min-h-[600px] overflow-hidden rounded-xl rounded-b-lg bg-background pb-6 shadow-2xl backdrop-blur-2xl animate-in sm:rounded-2xl md:min-h-[600px] md:w-[1050px]',
+                'min-h-[450px] overflow-hidden rounded-xl rounded-b-lg bg-background pb-6 shadow-2xl backdrop-blur-2xl animate-in sm:rounded-2xl md:min-h-[450px] md:w-[900px]',
               )}
             >
               <DialogTitle
@@ -251,7 +251,7 @@ export default function Settings({ open, onOpenChange, activeTab: initialTab }: 
                   <span className="sr-only">{localize('com_ui_close_settings')}</span>
                 </button>
               </DialogTitle>
-              <div className="max-h-[550px] overflow-auto px-6 md:max-h-[700px] md:min-h-[600px] md:w-[1050px]">
+              <div className="max-h-[550px] overflow-auto px-6 md:max-h-[550px] md:min-h-[450px] md:w-[900px]">
                 <Tabs.Root
                   value={activeTab}
                   onValueChange={handleTabChange}
