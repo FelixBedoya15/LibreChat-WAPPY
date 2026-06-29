8f852565fb765d22ab6244bf422b73c0c0b3a6fc
FelixBedoya15 <wappyinteractivo@gmail.com>
2026-06-29 18:47:12 -0500

    s

--- client/src/components/Nav/SettingsTabs/Account/GoogleDriveConnect.tsx
+++ client/src/components/Nav/SettingsTabs/Account/GoogleDriveConnect.tsx
@@ -122,10 +122,11 @@ export default function GoogleDriveConnect() {
         <div className="flex gap-3">
           {/* Custom Google Drive SVG Icon */}
           <div className="p-2 rounded-xl bg-gradient-to-br from-green-500/10 to-cyan-500/10 border border-green-500/20 flex-shrink-0 flex items-center justify-center w-10 h-10">
-            <svg viewBox="0 0 87.3 78" className="w-5 h-5" xmlns="http://www.w3.org/2000/svg">
-              <path d="m6.6 66.85 10.1-17.5h54l-10.1 17.5z" fill="#0f9d58"/>
-              <path d="m28.65 28.5 10.1-17.5h42l-10.1 17.5z" fill="#4285f4"/>
-              <path d="m16.7 49.35 22.05-38.35h20.2l-22.05 38.35z" fill="#f4b400"/>
+            <svg viewBox="0 0 24 24" className="w-5.5 h-5.5">
+              <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
+              <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
+              <path fill="#FBBC05" d="M5.84 14.1c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.08H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.92l2.85-2.22.81-.6z"/>
+              <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.08l3.66 2.84c.87-2.6 3.3-4.54 6.16-4.54z"/>
             </svg>
           </div>
           <div>
